5dff5e5da8a133c847b5bcf7b3794b2d954a8f5b
Anton Pavliuk <streetreadger@gmail.com>
2024-02-08 01:02:23 +0200

    Include template into program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use gtk::prelude::*;
+
+fn build_ui(app: &gtk::Application) {
+    let glade_src = include_str!("layout.xml");
+    let builder = gtk::Builder::from_string(glade_src);
+
+    let window: gtk::Window = builder.object(
+        "applicationwindow1"
+    ).unwrap();
+    window.set_application(Some(app));
+
+    window.show_all();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let application = gtk::Application::new(
+        Some("com.catsay-gui-glade"),
+        Default::default()
+    );
+
+    application.connect_activate(build_ui);
+
+    application.run();
 }
