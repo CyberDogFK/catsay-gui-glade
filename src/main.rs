54ae6117edfaef21b88859d59ff4c7631fbc5662
Anton Pavliuk <streetreadger@gmail.com>
2024-02-08 19:57:48 +0200

    Add interactive, and dead cat photo

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,50 @@ fn build_ui(app: &gtk::Application) {
     ).unwrap();
     window.set_application(Some(app));
 
+    // Inputs
+    let message_input: gtk::Entry = builder.object(
+        "message_input"
+    ).unwrap();
+
+    // Submit button
+    let button: gtk::Button = builder.object(
+        "generate_btn"
+    ).unwrap();
+
+    // Outputs
+    let message_output: gtk::Label = builder.object(
+        "message_output"
+    ).unwrap();
+
+    let is_dead_switch: gtk::Switch = builder.object(
+        "is_dead_switch"
+    ).unwrap();
+
+    let image_output: gtk::Image = builder.object(
+        "image_output"
+    ).unwrap();
+    let image_output_clone = image_output.clone();
+
+    button.connect_clicked(move |_| {
+        let is_dead = is_dead_switch.is_active();
+        if is_dead {
+            image_output_clone.set_from_file(
+                Some("./images/cat_dead.png")
+            )
+        } else {
+            image_output_clone.set_from_file(
+                Some("./images/cat.png")
+            )
+        }
+        message_output.set_text(&format!(
+            "{}\n \\\n \\",
+            message_input.text().to_lowercase()
+        ));
+        image_output_clone.show();
+    });
+
     window.show_all();
+    image_output.hide();
 }
 
 fn main() {
